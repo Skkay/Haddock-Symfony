6af7cd7b5eab92664db8611a2c619e5af922c2dc
Skkay <skkay.thomas@gmail.com>
2020-04-28 17:29:37 +0200

    Admin panel

---
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UtilisateurFixtures extends Fixture
+{
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new Utilisateur();
+        $user->setUsername('admin');
+        $user->setPassword($this->encoder->encodePassword($user, 'admin'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
